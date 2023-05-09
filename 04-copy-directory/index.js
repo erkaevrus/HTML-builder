fbb35e7318f927e6e4d94097c56006340e8eefe4
erkaevrus <erk_08@mail.ru>
2023-05-09 16:40:15 +0500

    fix: fix mistakes

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -3,17 +3,23 @@ const path = require('path')
 
 
 function copyDir() {
-  fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, err => {
-    if (err) throw err
-  })
+  fs.rm(path.join(__dirname, 'files-copy'), { recursive: true, force: true }, (err) => {
+    if (err) {throw err}
 
-  fs.readdir(path.join(__dirname, 'files'), (err, files) => {
-    if (err) throw err
-    for (let file of files) {
-      fs.copyFile(path.join(__dirname, `files/${String(file)}`), path.join(__dirname, `files-copy/${String(file)}`), err => {
+    fs.readdir(path.join(__dirname, 'files'), { withFileTypes: true }, (err, files) => {
         if (err) throw err
-      })
-    }
+
+        fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true, force: true }, err => {
+          if (err) throw err
+
+          files.forEach(file => {
+            fs.copyFile(path.join(__dirname, 'files', file.name), path.join(__dirname, 'files-copy', file.name), (err) => {
+              if (err) throw err
+            })
+          })
+        })
+      }
+    )
   })
 }
 
